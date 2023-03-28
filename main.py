1848bbc528b40bed723aa33f3e7d10ba4f33aecc
Maduki-tech <maduki.business@gmail.com>
2023-03-28 18:49:50 +0200

    work

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
+import json
+import asyncio
+from TikTokApi import TikTokApi
+from flask import Flask
 from src.getVideoByURL import getVideoByURL   
+import random
+import json
+from datetime import date
+# if __name__ == "__main__":
+#     getVideoByURL('https://www.tiktok.com/@aileensager/video/7212075500578147589')
 
-if __name__ == "__main__":
-    url = input("Enter URL: ")
-    getVideoByURL(url)
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    # return json.dumps({"error": "Please use /getVideoByURL?url=URL"})
+    DEVICE_ID = str(random.randint(10000, 99999999))
+# create new folder
+    # today = date.today()
+# os.mkdir(today.strftime("%d-%m-%Y"))
+    # slice url by last /
+    url = "https://www.tiktok.com/@aileensager/video/7212075500578147589"
+    id = url.split("/")[-1]
+    with TikTokApi(custom_device_id=DEVICE_ID) as api:
+        print(url)
+        video = api.video(id=id)
+        info_full = video.info_full()
+        json_object = json.dumps(info_full, indent=4)
+        #
+        # # print(video.info_full())
+        # with open("full_info.json", "w") as f:
+        #     f.write(json_object)
+        #
+        # # Bytes of the TikTok video
+        # video_data = video.bytes()
+        #
+        # with open("out.mp4", "wb") as out_file:
+        #     out_file.write(video_data)
+        if json_object != None:
+            return json.dumps(info_full)
+        else:
+            return json.dumps({"error": "Video not found"})
+    # data = getVideoByURL("https://www.tiktok.com/@aileensager/video/7212075500578147589")
+    # return data
+
+
+app.run()
+
--- src/getVideoByURL.py
+++ src/getVideoByURL.py
@@ -11,27 +11,23 @@ today = date.today()
 
 def getVideoByURL(url: str):
     # slice url by last /
-    try:
-        id = url.split("/")[-1]
-        print(id)
-        print(DEVICE_ID)
-        with TikTokApi(custom_device_id=DEVICE_ID) as api:
-            video = api.video(id=id)
-            info_full = video.info_full()
-            json_object = json.dumps(info_full, indent=4)
-
-            # print(video.info_full())
-            with open("full_info.json", "w") as f:
-                f.write(json_object)
-
-            # Bytes of the TikTok video
-            video_data = video.bytes()
-
-            with open("out.mp4", "wb") as out_file:
-                out_file.write(video_data)
-            if video_data != None:
-                return True
-            else:
-                return False
-    except Exception:
-        return False
+    id = url.split("/")[-1]
+    with TikTokApi(custom_device_id=DEVICE_ID) as api:
+        print(url)
+        video = api.video(id=id)
+        info_full = video.info_full()
+        json_object = json.dumps(info_full, indent=4)
+        #
+        # # print(video.info_full())
+        # with open("full_info.json", "w") as f:
+        #     f.write(json_object)
+        #
+        # # Bytes of the TikTok video
+        # video_data = video.bytes()
+        #
+        # with open("out.mp4", "wb") as out_file:
+        #     out_file.write(video_data)
+        if json_object != None:
+            return json.dumps(info_full)
+        else:
+            return json.dumps({"error": "Video not found"})
